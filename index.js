a1ae04d20093e5919413502caf012461c7d99aa8
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-01-10 21:03:32 +0530

    cors i

--- index.js
+++ index.js
@@ -88,8 +88,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin:["https://reach0ut.netlify.app","http://localhost:3000"],
-  credentials:true
+  origin:"*"
 }));app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //FILE STORAGE
