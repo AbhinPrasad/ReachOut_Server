e2f3d8f198c327b1e50424e801e36b88e51d4843
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-01-10 21:22:52 +0530

    corrrs

--- index.js
+++ index.js
@@ -88,8 +88,9 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin:"*"
-}));app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+  origin:["https://reach0ut.netlify.app","http://localhost:3000"],
+}));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //FILE STORAGE
 const storage = multer.diskStorage({
